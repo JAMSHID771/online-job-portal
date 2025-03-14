ba448e9adf0ae3f123c7b3fd13d3f7453306b1e8
Jamshid <jmamataliyev111@gmail.com>
2025-03-14 15:10:19 +0500

    this file defines all the routes in the web application

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [PageController::class, 'index'])->name('index');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
