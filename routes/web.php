dc7ae726348c0aeccca48433961e63a5ced287be
Jamshid <jmamataliyev111@gmail.com>
2025-03-14 11:53:46 +0500

    route name tog'irlandi

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/contact', [PageController::class, 'contact']);
-Route::get('/job-details', [PageController::class, 'jobDetails']);
-Route::get('/job-listings', [PageController::class, 'jobListings']);
+Route::get('/', [PageController::class, 'index'])->name('index');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/job-details', [PageController::class, 'jobDetails'])->name('job.details');
+Route::get('/job-listings', [PageController::class, 'jobListings'])->name('job.listings');
